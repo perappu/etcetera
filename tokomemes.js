ad4b48aa24c1adbf94a7a329f67444bd935906c1
Toko <stokoriel@gmail.com>
2022-06-24 06:58:51 -0500

    Update tokomemes.js

--- tokomemes.js
+++ tokomemes.js
@@ -1,124 +1,29 @@
-var image="https://i.imgur.com/5Xshc5x.png";  //Image path should be given here
-var no = 5; // No of images should fall
-var time = 10; // Configure whether image should disappear after x seconds (0=never):
-var speed = 50; // Fix how fast the image should fall
-var i, dwidth = 900, dheight =500; 
-var nht = dheight;
-var toppos = 0;
+// Fall v2.1 By MaxxBlade - http://www.maxxblade.co.uk/fall
+var fallObjects=new Array();function newObject(url,height,width){fallObjects[fallObjects.length]=new Array(url,height,width);}
+///////////// EDIT THIS SECTION //////////////
+var numObjs=5, waft=50, fallSpeed=15, wind=0;
+newObject("https://i.imgur.com/5Xshc5x.png",28,25);
+newObject("https://i.imgur.com/5Xshc5x.png",28,25);
+newObject("https://i.imgur.com/5Xshc5x.png",28,25);
+newObject("https://i.imgur.com/5Xshc5x.png",21,21);
+newObject("https://i.imgur.com/5Xshc5x.png",21,21);
+//////////////////////////////////////////////
 
-if(document.all){
-	var ie4up = 1;
-}else{
-	var ie4up = 0;
+function winSize(){winWidth=(moz)?window.innerWidth:document.body.clientWidth;winHeight=(moz)?window.innerHeight:document.body.clientHeight;}
+function winOfy(){winOffset=(moz)?window.pageYOffset:document.body.scrollTop;}
+function fallObject(num,vari,nu){
+	objects[num]=new Array(parseInt(Math.random()*(winWidth-waft)),-30,(parseInt(Math.random()*waft))*((Math.random()>0.5)?1:-1),0.02+Math.random()/20,0,1+parseInt(Math.random()*fallSpeed),vari,fallObjects[vari][1],fallObjects[vari][2]);
+	if(nu==1){document.write('<img id="fO'+i+'" style="position:absolute;" src="'+fallObjects[vari][0]+'">'); }
 }
-
-if(document.getElementById && !document.all){
-	var ns6up = 1;
-}else{
-	var ns6up = 0;
-}
-
-function getScrollXY() {
-  var scrOfX = 10, scrOfY = 10;
-  if( typeof( window.pageYOffset ) == 'number' ) {
-    //Netscape compliant
-    scrOfY =window.pageYOffset;
-    scrOfX = window.pageXOffset;
-  } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
-    //DOM compliant
-    scrOfY = document.body.scrollTop;
-    scrOfX = document.body.scrollLeft;
-  } else if( document.documentElement &&
-      ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
-    //IE6 standards compliant mode
-   scrOfY = document.documentElement.scrollTop;
-   scrOfX = document.documentElement.scrollLeft;
-  }
-  return [ scrOfX, scrOfY ];
-}
-
-var timer;
-
-function ranrot()
-{
-
-var a = getScrollXY()
-if(timer)
-{
-	clearTimeout(timer);
-}
-toppos = a[1];
-dheight = nht+a[1];
-//alert(dheight);
-
-timer = setTimeout('ranrot()',2000);
-}
- 	
-ranrot();
- 	
-function iecompattest()
-{
-	if(document.compatMode && document.compatMode!="BackCompat")
-	{
-		return document.documentElement;
-	}else{
-		return document.body;
+function fall(){
+	for(i=0;i<numObjs;i++){
+		var fallingObject=document.getElementById('fO'+i);
+		if((objects[i][1]>(winHeight-(objects[i][5]+objects[i][7])))||(objects[i][0]>(winWidth-(objects[i][2]+objects[i][8])))){fallObject(i,objects[i][6],0);}
+		objects[i][0]+=wind;objects[i][1]+=objects[i][5];objects[i][4]+=objects[i][3];
+		with(fallingObject.style){ top=objects[i][1]+winOffset+"px";left=objects[i][0]+(objects[i][2]*Math.cos(objects[i][4]))+"px";}
 	}
-	
+	setTimeout("fall()",31);
 }
-if (ns6up) {
-	dwidth = window.innerWidth;
-	dheight = window.innerHeight;
-} 
-else if (ie4up) {
-	dwidth = iecompattest().clientWidth;
-	dheight = iecompattest().clientHeight;
-}
-
-nht = dheight;
-
-var cv = new Array();
-var px = new Array();       //position variables
-var py = new Array();      //position variables
-var am = new Array();     //amplitude variables
-var sx = new Array();    //step variables
-var sy = new Array();   //step variables
-
-for (i = 0; i < no; ++ i) {  
-	cv[i] = 0;
-	px[i] = Math.random()*(dwidth-100);  // set position variables
-	py[i] = Math.random()*dheight;     // set position variables
-	am[i] = Math.random()*20;         // set amplitude variables
-	sx[i] = 0.02 + Math.random()/10;  // set step variables
-	sy[i] = 0.7 + Math.random();    // set step variables
-	document.write("<div id=\"dot"+ i +"\" style=\"POSITION: absolute; Z-INDEX: "+ i +"; VISIBILITY: visible; TOP: 15px;LEFT: 15px;\"><img src='"+image+"' border=\"0\"><\/div>");
-}
-
-function animation() {  // animation function
-	for (i = 0; i < no; ++ i) {  // iterate for every dot
-		py[i] += sy[i];
-      		if (py[i] > dheight-50) {
-        		px[i] = Math.random()*(dwidth-am[i]-100);
-        		py[i] = toppos;
-        		sx[i] = 0.02 + Math.random()/10;
-        		sy[i] = 0.7 + Math.random();
-      		}
-      		cv[i] += sx[i];
-      		document.getElementById("dot"+i).style.top=py[i]+"px";
-      		document.getElementById("dot"+i).style.left=px[i] + am[i]*Math.sin(cv[i])+"px";  
-    	}
-    	atime=setTimeout("animation()", speed);
-
-}
-
-function hideimage(){
-	if (window.atime) clearTimeout(atime)
-		for (i=0; i<no; i++) 
-			document.getElementById("dot"+i).style.visibility="hidden"
-}
-if (ie4up||ns6up){
-animation();
-if (time>0)
-	setTimeout("hideimage()", time*1000)
-}
-animation();
+var objects=new Array(),winOffset=0,winHeight,winWidth,togvis,moz=(document.getElementById&&!document.all)?1:0;winSize();
+for (i=0;i<numObjs;i++){fallObject(i,parseInt(Math.random()*fallObjects.length),1);}
+window.onscroll=winOfy;window.onresize=winSize;fall();
